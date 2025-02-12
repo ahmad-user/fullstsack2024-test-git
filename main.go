3d9624a8ec5444386f2c7190fb49abd47d61a8fb
Fadli <1119110006@global.ac.id>
2025-02-12 09:48:34 +0700

    update

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
+// package main
+
+// import "fmt"
+
+// type Employee struct {
+// 	Name   string
+// 	Salary float64
+// }
+
+// func (e *Employee) increaseSalary(percentage float64) {
+// 	e.Salary += e.Salary * percentage / 100
+// }
+
+// func main() {
+// 	emp := Employee{Name: "John", Salary: 5000}
+// 	emp.increaseSalary(10)
+// 	fmt.Println(emp.Salary)
+
+// }
+
+// Soal no 4
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func printNumbers() {
+// 	for i := 1; i <= 5; i++ {
+// 		fmt.Println(i)
+// 		time.Sleep(100 * time.Millisecond)
+// 	}
+// }
+
+// func main() {
+// 	go printNumbers()
+// 	fmt.Println("selesai")
+// }
+
+// soal 5
+
 package main
 
-import "fmt"
-
-type Employee struct {
-	Name   string
-	Salary float64
-}
-
-func (e *Employee) increaseSalary(percentage float64) {
-	e.Salary += e.Salary + percentage/100
-}
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	emp := Employee{Name: "John", Salary: 5000}
-	emp.increaseSalary(10)
-	fmt.Println(emp.Salary)
-
+	s := "go is amaring"
+	words := strings.Split(s, " ")
+	fmt.Println(len(words))
 }
